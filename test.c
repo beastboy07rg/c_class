2c4de47da078a94e2b2413d68374522c37c1cd08
Your Name <you@example.com>
2024-11-13 15:47:36 +0530

    fourth commit

--- test.c
+++ test.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main()
 {
-	printf("hello girish");
-	printf("yesu yesu mera yesu");
+      int hra , da , basic , gross ;
+      printf("enter the basic pay of an employee");
+      scanf("%d",&basic);
 
-	printf("juice pilado");
-	printf("comeback");
-	printf("New commit\n");
-	printf("inside branch1, introducing conflict");
-	printf("girish owns this branch");
+      hra = .20*basic;
+	      da = 1.50*basic;
+	      gross = da + hra + basic ;
+
+	      printf("the basic salary of an employee  is %d",gross);
+printf("you are all set");
 	return 0 ;
 }
